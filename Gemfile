a43cb80d0ef6a1bdc831a59da5d18865c39c5b3c
Abhishek Dave <abhidave2000@gmail.com>
2026-05-03 07:58:53 -0400

    Fix RuboCop lint issues

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source "https://rubygems.org"
 
 # Gemfile
-gem "dotenv-rails", groups: [:development, :test]  # ← must be BEFORE other gems
+gem "dotenv-rails", groups: [ :development, :test ]  # ← must be BEFORE other gems
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 ruby "3.3.4"
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,11 @@ class User < ApplicationRecord
 
     has_many :job_applications, dependent: :destroy
 
-    validates :email, presence: true, uniqueness: { case_sensitive: false }, 
+    validates :email, presence: true, uniqueness: { case_sensitive: false },
                         format: { with: URI::MailTo::EMAIL_REGEXP }
 
     validates :first_name, presence: true
     validates :last_name, presence: true
 
-    before_save {self.email = email.downcase}
+    before_save { self.email = email.downcase }
 end
